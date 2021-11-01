975e4d211f6683df945462a6c94f1ded64cdba57
HoangDucPhuong0404 <phuongduc826@gmail.com>
2021-11-01 16:00:38 +0700

    bosungcau2js

--- thuchanh/cau2.js
+++ thuchanh/cau2.js
@@ -2,11 +2,12 @@
 function checknumber() {
     let arr = [1, 2, 5, 6, 8, 9, 4];
     let number = document.getElementById("textbox").value;
+    console.log(checkNumberinArr(number,arr))
     if(checkNumberinArr(number,arr) != false){
-        alert(`vi tri ${number} `)
+        alert(`vi tri ${number} `);
     }
     else {
-        alert(`${number}  nam trong mang`)
+        alert(`${number} khong nam trong mang ${arr.join(' - ')}`);
     }
 }
     function checkNumberinArr(number,arr) {
